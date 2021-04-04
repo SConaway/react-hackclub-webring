75af5efe2987af49654b7dc6ded9b6eb2c3f8c26
Steven Conaway <steven.conaway@icloud.com>
2021-04-03 17:05:39 -0700

    Add base component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'hackclub-webring'
+import { HackClubWebring } from 'hackclub-webring'
 import 'hackclub-webring/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <HackClubWebring />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const HackClubWebring = ({}) => {
+  return (
+    <div className={styles.wrapper}>
+      <a
+        href='https://webring.hackclub.com/'
+        id='previousBtn'
+        className={styles.anchor}
+        title='Previous'
+      >
+        ‹
+      </a>
+      <a
+        href='https://webring.hackclub.com/'
+        className={styles.logo}
+        title='Hack Club Webring'
+        alt='Hack Club Webring'
+      ></a>
+      <a
+        href='https://webring.hackclub.com/'
+        id='nextBtn'
+        className={styles.anchor}
+        title='Next'
+      >
+        ›
+      </a>
+    </div>
+  )
 }
